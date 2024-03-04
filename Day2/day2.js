f21af8df39e54c7a78857b213130783419539483
Abid Bhundi <160416081+DevAbidAslam@users.noreply.github.com>
2024-03-04 20:56:05 +0500

    Add files via upload

---
+++ Day2/day2.js
@@ -0,0 +1,10 @@
+//Question 4
+console.log("Nelson Mandela once said, 'The greatest glory in living lies not in never falling, but in rising every time we fall'");
+//Question 5
+var famous_person = "Nelson Mandela";
+var message = "".concat(famous_person, " once said, 'The greatest glory in living lies not in never falling, but in rising every time we fall'");
+console.log(message);
+//Question 6
+var Name = "\t\n Abid \t\n";
+console.log(Name);
+console.log(Name.trim());
