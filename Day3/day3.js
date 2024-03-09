4b41c8179735ce8b498d70c26e4a3a7c03ae1e1a
Abid Bhundi <160416081+DevAbidAslam@users.noreply.github.com>
2024-03-09 22:49:43 +0500

    Add files via upload

---
+++ Day3/day3.js
@@ -0,0 +1,11 @@
+// Day3 of 100days of Coding Challenge
+/* Question 7&8
+Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.*/
+console.log("Addition: ".concat(5 + 3));
+console.log("Subtraction: ".concat(10 - 2));
+console.log("Multiplication: ".concat(2 * 4));
+console.log("Division: ".concat(16 / 2));
+/* Question 9
+Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.*/
+var favourite_number = 15;
+console.log("My favourite number is ".concat(favourite_number));
---
+++ Day3/day3.ts
@@ -0,0 +1,16 @@
+// Day3 of 100days of Coding Challenge
+
+/* Question 7&8
+Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.*/
+
+console.log(`Addition: ${5+3}`);
+console.log(`Subtraction: ${10-2}`);
+console.log(`Multiplication: ${2*4}`);
+console.log(`Division: ${16/2}`);
+
+/* Question 9
+Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.*/
+
+let favourite_number : number = 15
+console.log(`My favourite number is ${favourite_number}`);
+
